c58a7c0ab5162ed93f2306f6451bd232d080544b
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-15 21:28:43 +0300

    commit tree 82bbfc42152da2700c9f17604f602296b36cdcbd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-const init = () => {
+const init = () =>
+{
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener("submit", (eventFunction) => {
+        eventFunction.preventDefault();
+        const input = document.querySelector( "input#searchByID" );
+        
+        console.log( eventFunction );
+
+        fetch( `http://localhost:3000/movies/${input.value}` )
+            .then( response => response.json() )
+            .then( data =>
+            {
+                 const title = document.querySelector(
+                   "section#movieDetails h4"
+                 );
+                 const summary = document.querySelector(
+                   "section#movieDetails p"
+                 );
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+                console.log( data );
+            } );
+    });
   
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener( 'DOMContentLoaded', init );
